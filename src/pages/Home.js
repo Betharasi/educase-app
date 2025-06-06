f2a5bf4243ca1394652aa52ac834b0dc2bde9df8
Betharasi Tarun <betharasitarun@gmail.com>
2025-06-06 16:03:36 +0530

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-green-100">
+      <h1 className="text-3xl font-bold text-green-700">Welcome to the Home Page!</h1>
+    </div>
+  );
+}
+
+export default Home;
